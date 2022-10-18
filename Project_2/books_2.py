019aaf79e750f770c6a29a1f052edc6de6bb7024
eduardo-ferroni <eduard.ferroni@hp.com>
2022-10-18 10:40:30 -0300

    update

--- Project_2/books_2.py
+++ Project_2/books_2.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request, status
 from pydantic import BaseModel, Field
 from uuid import UUID
 from typing import Optional
+from starlette.responses import JSONResponse
+
+
+class NegativeNumberException(Exception):
+    def __init__(self, books_to_return):
+        self.books_to_return = books_to_return
+
 
 app = FastAPI()
 
@@ -27,21 +34,49 @@ class Book(BaseModel):
         }
 
 
+class BookNoRating(BaseModel):
+    id: UUID
+    title: str = Field(min_length=1, max_length=100)
+    author: str = Field(min_length=1, max_length=100)
+    description: Optional[str] = Field(title="Description of the book",
+                                       min_length=1,
+                                       max_length=100)
+
+
 BOOKS = []
 
 
+@app.exception_handler(NegativeNumberException)
+def negative_number_exception_handler(request: Request, exception: NegativeNumberException):
+    return JSONResponse(
+        status_code=418,
+        content={"message": f"negative number"}
+    )
+
+
 @app.get("/")
-def read_all_books(book_to_return: Optional[int] = None):
+def read_all_books(books_to_return: Optional[int] = None):
+    if books_to_return and books_to_return < 0:
+        raise NegativeNumberException(books_to_return=books_to_return)
+
     if len(BOOKS) < 1:
         create_books_no_api()
 
-    if book_to_return is not None and len(BOOKS) >= book_to_return > 0:
-        return BOOKS[0:book_to_return]
+    if books_to_return is not None and len(BOOKS) >= books_to_return > 0:
+        return BOOKS[0:books_to_return]
     return BOOKS
 
 
 @app.get("/book/{book_id}")
 def read_book(book_id: UUID):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+    raise raise_item_cannot_be_found_exception()
+
+
+@app.get("/book/rating/{book_id}", response_model=BookNoRating)
+def read_book_no_rating(book_id: UUID):
     for book in BOOKS:
         if book.id == book_id:
             return book
@@ -66,7 +101,7 @@ def delete_book(book_id: UUID):
     raise raise_item_cannot_be_found_exception()
 
 
-@app.post("/")
+@app.post("/", status_code=status.HTTP_201_CREATED)
 def create_book(book: Book):
     BOOKS.append(book)
     return book
